57249e9731ae1428806e51800c0b9b89eb4b8719
sushree mishra <sushree022@gmail.com>
2019-07-11 14:58:58 +0530

    solution 9 added

---
+++ src/main/java/com/stackroute/Rev_String.java
@@ -0,0 +1,19 @@
+package com.stackroute;
+import java.util.Scanner;
+
+public class Rev_String {
+    public static void main (String[]args)
+    {
+        Scanner input= new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String word= input.nextLine();
+        int length=word.length();
+        String rev_word= " ";
+        for(int i = word.length() - 1; i >= 0; i--)
+        {
+            rev_word= rev_word+ word.charAt(i);
+        }
+        System.out.println("The reverse string is" + rev_word);
+    }
+
+}
