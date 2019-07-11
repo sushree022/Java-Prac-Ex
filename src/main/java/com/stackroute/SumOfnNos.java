e60a2078813125069accab8141bfb72a7a9a6e7a
sushree mishra <sushree022@gmail.com>
2019-07-11 11:00:52 +0530

    solution 5 added

---
+++ src/main/java/com/stackroute/SumOfnNos.java
@@ -0,0 +1,21 @@
+package com.stackroute;
+import java.util.Scanner;
+
+public class SumOfnNos {
+    public static void main(String[]args)
+    {
+        Scanner input= new Scanner(System.in);
+        int n, sum=0;
+        System.out.print("How many numbers you want to enter: ");
+        n= input.nextInt();
+        int a[]= new int[n];
+        System.out.print("Enter the numbers: ");
+        for(int i=0; i<n; i++)
+        {
+            a[i]= input.nextInt();
+            sum= sum+ a[i];
+        }
+        System.out.println("Sum of all numbers is: "+ sum);
+    }
+}
+
