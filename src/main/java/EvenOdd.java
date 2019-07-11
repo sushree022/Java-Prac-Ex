9f5339920c41cf47408fba3475d6851d6c51f6fa
sushree mishra <sushree022@gmail.com>
2019-07-11 09:49:09 +0530

    solution 2 added

---
+++ src/main/java/EvenOdd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class EvenOdd {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num= input.nextInt();
+        if(num>20 && num<30) {
+           if(num % 2==0){
+                System.out.println("Jerry");
+            } else
+                System.out.println("Tom");
+        }else
+            System.out.println("Enter number between 20 and 30");
+    }
+}
